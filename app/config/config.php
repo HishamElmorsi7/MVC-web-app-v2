b074e18d5cb11407365d775382d250caf90874c7
HishamElmorsi7 <hi7u3xe@gmail.com>
2022-03-31 19:11:55 +0200

    task

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+//paths info
+define('APP_PATH', realpath(dirname(dirname(__FILE__))));
+define('VIEWS_PATH', APP_PATH . DIRECTORY_SEPARATOR . 'views');
+
+
+//Connection info
+define('DB_HOST', 'localhost');
+define('USER', 'id18252991_hishamelmorsi');
+define('PWD', 'lplhjs890098##*H');
+define('DB_NAME', 'id18252991_products');
+define('SITE_NAME', 'task');
---
+++ app/views/notfound/notfound.view.php
@@ -0,0 +1,4 @@
+
+<h1 style= "color:#887D7D">  N O T FOUND P A G E :) </h1>
+
+Go To <a href="/" style= "text-decoration: none; color: #BF0909; font-size: 20px"> Home</a>
